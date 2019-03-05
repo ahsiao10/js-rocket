718c88dc98a64a8eecee4e7ada88770a1bf4b6da
Annie <anniehsiao55@hotmail.com>
2019-03-04 16:56:53 -0800

    MAde some car stuff happen

---
+++ explanation.js
@@ -0,0 +1,51 @@
+// explanations: variables
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true; 
+var annieObject = {
+	firstName: 'Annie',
+	lastName: 'Hsiao',
+	favColor: ['blue','red', 'green'],
+	yearsAlive: 103;
+	isFemale: true
+}
+
+//functions 
+
+var fullName = function (firstName, secondName) {
+	return firstName + '' + secondName; 
+}
+
+var area = function (width, height) {
+	return width * height; 
+}
+
+
+var whatIsMyFavColor = function () {
+	
+	return false;
+
+	return true;
+
+	return 'blue';
+
+}
+
+var doSomething = function () {
+	console.log("do Something!!!")
+}
+
+
+// explnations what if statements 
+
+var number1 = 10;
+var number2 = 100;
+
+if (number1 < number2 ) {
+	alert('ture!!! ');
+}; else {
+	alert('false!!!');
+}
+
+
